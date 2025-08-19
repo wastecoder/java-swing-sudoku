687ed7368c2ddcaab55955e6aed1fbc50eaeadd2
Pedro <pedromendes7182@gmail.com>
2025-08-19 11:06:06 -0300

    feat: finaliza o sudoku via terminal

---
+++ src/br/com/sudoku/model/Space.java
@@ -0,0 +1,39 @@
+package br.com.sudoku.model;
+
+public class Space {
+
+    private Integer actual;
+    private final int expected;
+    private final boolean fixed;
+
+
+    public Space(final int expected, final boolean fixed) {
+        this.expected = expected;
+        this.fixed = fixed;
+        if (fixed){
+            actual = expected;
+        }
+    }
+
+
+    public void clearSpace(){
+        setActual(null);
+    }
+
+    public Integer getActual() {
+        return actual;
+    }
+
+    public void setActual(final Integer actual) {
+        if (fixed) return;
+        this.actual = actual;
+    }
+
+    public int getExpected() {
+        return expected;
+    }
+
+    public boolean isFixed() {
+        return fixed;
+    }
+}
